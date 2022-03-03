ed063b3b87b01096225a82b0a3d5cc453084a6d9
Bernardo <bmg@envira.es>
2022-03-03 08:50:23 +0100

    Removed unused imports

--- src/main/resources/archetype-resources/src/main/java/model/DefaultExampleEntity.java
+++ src/main/resources/archetype-resources/src/main/java/model/DefaultExampleEntity.java
@@ -24,14 +24,6 @@
 
 package ${package}.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-
 import lombok.Data;
 
 /**
